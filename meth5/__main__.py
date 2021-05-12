956f2f4d2d2feee6b1f1ba9f33e66b7ca3998431
snajder <rene.snajder@gmail.com>
2021-05-12 13:48:51 +0200

    reformatting

--- meth5/__main__.py
+++ meth5/__main__.py
@@ -3,31 +3,64 @@ import pathlib
 
 import meth5.main_create_h5
 
+
 def main():
-    parser = argparse.ArgumentParser(
-        description="MetH5 file tools")
+    parser = argparse.ArgumentParser(description="MetH5 file tools")
     
-    parser.add_argument("--chunk_size", type=int, required=False, default=int(1e6),
-        help="Number of llrs per chunk", )
+    parser.add_argument(
+        "--chunk_size",
+        type=int,
+        required=False,
+        default=int(1e6),
+        help="Number of llrs per chunk",
+    )
     
     subparsers = parser.add_subparsers(description="Subcommand: ", dest="subcommand")
     subparsers.required = True
     
-    sc_args = subparsers.add_parser("create_h5",
-        description="Create H5 file from Nanopolish result files", )
+    sc_args = subparsers.add_parser(
+        "create_h5",
+        description="Create H5 file from Nanopolish result files",
+    )
     sc_args.set_defaults(func=meth5.main_create_h5.main)
     
-    sc_args.add_argument("--input_dir", type=pathlib.Path, required=True,
-        help="Input directory containing Nanopolish result files", )
-
-    sc_args.add_argument("--output_file", type=pathlib.Path, required=True,
-                         help="Output MetH5 file", )
-
-    sc_args.add_argument("--quiet", action="store_true", help="No progress bar or warnings will be displayed", )
-
-    sc_args.add_argument("--compression", type=str, required=False, default="gzip",
-                         choices = ["gzip", "None"],
-                         help="Compression method for the MetH5 data structures. Use 'gzip' for smaller file size, or 'None' for faster read and write speeds")
+    sc_args.add_argument(
+        "--input_dir",
+        type=pathlib.Path,
+        required=True,
+        help="Input directory containing Nanopolish result files",
+    )
+    
+    sc_args.add_argument(
+        "--output_file",
+        type=pathlib.Path,
+        required=True,
+        help="Output MetH5 file",
+    )
+    
+    sc_args.add_argument(
+        "--quiet",
+        action="store_true",
+        help="No progress bar or warnings will be displayed",
+    )
+    
+    sc_args.add_argument(
+        "--compression",
+        type=str,
+        required=False,
+        default="gzip",
+        choices=["gzip", "None"],
+        help="Compression method for the MetH5 data structures. Use 'gzip' for smaller file size, or 'None' for faster read and write speeds",
+    )
+    
+    sc_args.add_argument(
+        "--allowed_chromosomes",
+        type=str,
+        nargs="+",
+        required=False,
+        default=None,
+        help="Only include these chromosomes",
+    )
     
     args = parser.parse_args()
     args_dict = vars(args)
--- meth5/main_create_h5.py
+++ meth5/main_create_h5.py
@@ -1,12 +1,19 @@
+from typing import List
 import argparse
 import tqdm
 from pathlib import Path
 from meth5.meth5 import MetH5File
 
-def main(chunk_size: int, input_dir: Path, output_file: Path, compression: str, quiet: bool):
+
+def main(
+    chunk_size: int, input_dir: Path, output_file: Path, compression: str, allowed_chromosomes: List[str], quiet: bool
+):
     if compression == "None":
         compression = None
-        
+    
+    if allowed_chromosomes is not None and len(allowed_chromosomes) == 0:
+        allowed_chromosomes = None
+    
     input_files = list(input_dir.iterdir())
     
     if len(input_files) == 0:
@@ -14,5 +21,6 @@ def main(chunk_size: int, input_dir: Path, output_file: Path, compression: str, 
     
     with MetH5File(output_file, chunk_size=chunk_size, mode="w", compression=compression) as m5_out:
         for input_file in tqdm.tqdm(input_files) if not quiet else input_files:
-            m5_out.parse_and_add_nanopolish_file(input_file, postpone_sorting_until_close=True)
-            
+            m5_out.parse_and_add_nanopolish_file(
+                input_file, postpone_sorting_until_close=True, include_chromosomes=allowed_chromosomes
+            )
